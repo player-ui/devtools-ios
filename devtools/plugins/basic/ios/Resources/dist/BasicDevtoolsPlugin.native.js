fe169ddd8c283e419b84439d75329095fd4ac316
Intuit Service <opensource-svc@intuit.com>
2026-09-18 14:14:30 -0700

    Version 0.15.1--canary.29.1254

--- devtools/plugins/basic/ios/Resources/dist/BasicDevtoolsPlugin.native.js
+++ devtools/plugins/basic/ios/Resources/dist/BasicDevtoolsPlugin.native.js
@@ -1441,7 +1441,7 @@ var BasicDevtoolsPlugin = function() {
             playerConfig: {}
         }
     };
-    var PLUGIN_VERSION = true ? "0.15.1" : "unstamped";
+    var PLUGIN_VERSION = true ? "0.15.1--canary.29.1254" : "unstamped";
     var _obj;
     var BasicPluginData = {
         id: PLUGIN_ID,
--- devtools/plugins/profiler/ios/Resources/dist/ProfilerDevtoolsPlugin.native.js
+++ devtools/plugins/profiler/ios/Resources/dist/ProfilerDevtoolsPlugin.native.js
@@ -1685,7 +1685,7 @@ var ProfilerDevtoolsPlugin = function() {
             rawNodes: []
         }
     };
-    var PLUGIN_VERSION = true ? "0.15.1" : "unstamped";
+    var PLUGIN_VERSION = true ? "0.15.1--canary.29.1254" : "unstamped";
     var _obj;
     var ProfilerPluginData = {
         id: PLUGIN_ID,
