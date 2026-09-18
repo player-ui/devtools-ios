ee790283de5edf51d7f077a29dc6929d06492298
Intuit Service <opensource-svc@intuit.com>
2026-09-18 14:10:48 -0700

    Version 0.15.1

--- devtools/plugins/basic/ios/Resources/dist/BasicDevtoolsPlugin.native.js
+++ devtools/plugins/basic/ios/Resources/dist/BasicDevtoolsPlugin.native.js
@@ -1441,7 +1441,7 @@ var BasicDevtoolsPlugin = function() {
             playerConfig: {}
         }
     };
-    var PLUGIN_VERSION = true ? "0.15.0" : "unstamped";
+    var PLUGIN_VERSION = true ? "0.15.1" : "unstamped";
     var _obj;
     var BasicPluginData = {
         id: PLUGIN_ID,
