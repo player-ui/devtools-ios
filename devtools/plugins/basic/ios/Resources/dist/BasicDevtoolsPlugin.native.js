ccbb5f74a28451c887c408172e3cd4f0d4f0bc10
Intuit Service <opensource-svc@intuit.com>
2026-09-22 07:39:15 -0700

    Version 0.15.2-next.0

--- devtools/plugins/basic/ios/Resources/dist/BasicDevtoolsPlugin.native.js
+++ devtools/plugins/basic/ios/Resources/dist/BasicDevtoolsPlugin.native.js
@@ -1441,7 +1441,7 @@ var BasicDevtoolsPlugin = function() {
             playerConfig: {}
         }
     };
-    var PLUGIN_VERSION = true ? "0.15.1" : "unstamped";
+    var PLUGIN_VERSION = true ? "0.15.2-next.0" : "unstamped";
     var _obj;
     var BasicPluginData = {
         id: PLUGIN_ID,
--- devtools/plugins/profiler/ios/Resources/dist/ProfilerDevtoolsPlugin.native.js
+++ devtools/plugins/profiler/ios/Resources/dist/ProfilerDevtoolsPlugin.native.js
@@ -1685,7 +1685,7 @@ var ProfilerDevtoolsPlugin = function() {
             rawNodes: []
         }
     };
-    var PLUGIN_VERSION = true ? "0.15.1" : "unstamped";
+    var PLUGIN_VERSION = true ? "0.15.2-next.0" : "unstamped";
     var _obj;
     var ProfilerPluginData = {
         id: PLUGIN_ID,
